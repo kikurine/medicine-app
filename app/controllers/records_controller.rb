079e21631de7044792fb113073994b7b7badca85
kikurine <mariko-kikunaga@gaji.jp>
2026-03-12 01:31:41 +0900

    welcome>records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,11 +1,5 @@
-class RecordsController < ApplicationController
-  def index
-    @medicines = Medicine.all # 「記事を全部取ってきて、@articlesに入れて！」という命令
-  end
-  # 追加：ボタンが押された時の処理
-  def record_dose
-    medicine = Medicine.find(params[:id])
-    medicine.dose_records.create! # 新しい記録を保存
-    redirect_to root_path, notice: "#{medicine.name}の服用を記録しました！"
-  end
+# ✅ 修正後
+def index
+  @records = Record.all
+  # これだけでOK！ Railsは自動的に index.html.erb を探しに行きます
 end
